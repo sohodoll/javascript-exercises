9847445b08bfd461b86ce39931a420feb128d579
sohodoll <kovalevvtl@gmail.com>
2022-10-24 06:50:13 +0300

    docs: solve 2nd JS exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let result = '';
 
+    if (num < 0 ) {
+        return 'ERROR'
+    }
+
+    if (str === '') {
+        return str;
+    }
+
+    for(let i = 0; i<num; i++) {
+        result = result + str;
+    }
+    return result;
 };
 
 // Do not edit below this line
