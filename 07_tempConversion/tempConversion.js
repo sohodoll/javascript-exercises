79b76ee4a12a8f54df9723f8476ae2e56724a1ed
sohodoll <kovalevvtl@gmail.com>
2022-10-24 15:43:33 +0300

    feat: solve 7th JS task (temperature)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(faren) {
+  const cels = (faren - 32) * 5/9;
+  const result = Math.round(cels * 10) / 10;
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(cels) {
+  const faren = (cels * 9/5) + 32;
+  const result = Math.round(faren * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
