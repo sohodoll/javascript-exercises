bec280d69fb69fc36ad30774e504b936d44922f5
sohodoll <kovalevvtl@gmail.com>
2022-10-31 15:00:57 +0300

    feat: finish palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let trimmedStr = str.split(',').join('')
+    .split('!').join('')
+    .split(' ').join('')
+    .replace('.', '').toLowerCase();
+    let reversedStr = '';
+    for (let i = trimmedStr.length-1; i >= 0; i--) {
+        reversedStr += trimmedStr[i];
+    }
+    return reversedStr==trimmedStr;
 };
 
 // Do not edit below this line
