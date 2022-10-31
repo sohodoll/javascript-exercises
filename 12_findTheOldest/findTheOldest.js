bca6044978283925a892cf69b8153cf6f995ad2a
sohodoll <kovalevvtl@gmail.com>
2022-10-31 15:35:11 +0300

    feat: solve 12th task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.forEach(person => {
+        if(!person.yearOfDeath) {
+            person.yearOfDeath = (new Date().getFullYear());
+        }
+        person['yearsLived'] = person.yearOfDeath - person.yearOfBirth;
+    })
+    array.sort((a, b) => {
+        return a.yearsLived > b.yearsLived ? -1 : 1
+    })
+    return array[0];
 };
 
 // Do not edit below this line
