19d2b22fcad31ef2cafd8f63d6835e138f307bcf
sohodoll <kovalevvtl@gmail.com>
2022-10-31 15:20:59 +0300

    feat: solve 11th task

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let newArray = [];
+    for (let i = 0; i< array.length; i++) {
+        newArray.push(array[i].title);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
