1f2835c3edb05cb56f35245f66dbc32667a84f5c
sohodoll <kovalevvtl@gmail.com>
2022-10-31 15:18:16 +0300

    feat: solve 10th task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n<0) {
+        return 'OOPS'
+    }
+    n = Number(n);
+    let array = [];
+    for (let i = 1; i <= n; i++) {
+        if (i == 1) {
+            array.push(1);
+        } else if (i == 2) {
+          array.push(1);  
+        } else {
+            array.push(array[i-2] + array[i-3]);
+        }
+    }
+    return array[n-1];
 };
 
 // Do not edit below this line
