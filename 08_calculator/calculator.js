eddc540676edccdf60978411cc210993e617d2db
sohodoll <kovalevvtl@gmail.com>
2022-10-31 14:46:35 +0300

    feat: finish 8th task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (!array.length) {return 0}
+	const result =  array.reduce((total, element) => {
+      return total + element;
+    })
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (!array.length) {return 0}
+	const result =  array.reduce((total, element) => {
+      return total * element;
+    })
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+   for (let i = 1; i <= a; i++) {
+       result = result * i;
+   }
+   return result;
 };
 
 // Do not edit below this line
